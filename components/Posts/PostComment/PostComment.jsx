dbe2e2de4c204fff142b24d195ba2773fd1a072c
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-18 17:23:57 +0200

    corrigiendo display de comentarios/respuestas

--- components/Posts/PostComment/PostComment.jsx
+++ components/Posts/PostComment/PostComment.jsx
@@ -21,30 +21,32 @@ export default function PostComment({ comment }) {
 					<p className='comment--author'>
 						<span>{comment.username}</span>: {comment.text}
 					</p>
-					{comment.responses.length > 0 ? (
-						<p
-							onClick={switchingGeneric}
-							className='response--parraf'
-						>{`Leer respuetas (${comment.responses.length})`}</p>
-					) : (
-						<p className='response--parraf'>Responder</p>
-					)}
-					{comment.author_comment.id == user.id && (
-						<p onClick={handleDelete} className='response--parraf'>
-							Eliminar
-						</p>
-					)}
+					<div className='comment--container--options'>
+						{comment.responses.length > 0 ? (
+							<p
+								onClick={switchingGeneric}
+								className='response--parraf'
+							>{`Leer respuetas (${comment.responses.length})`}</p>
+						) : (
+							<p className='response--parraf'>Responder</p>
+						)}
+						{comment.author_comment.id == user.id && (
+							<p onClick={handleDelete} className='response--parraf'>
+								Eliminar
+							</p>
+						)}
+					</div>
 				</>
 			)}
 			{isTrue && comment.responses.length > 0 && (
 				<div className={`post--card--response--container--${isTrue}`}>
-					<p className='comment--author'>
+					{/* <p className='comment--author'>
 						<span>{comment.username}</span>: {comment.text}
-					</p>
+					</p> */}
 					{comment.responses.map((response) => (
 						<Response response={response} key={response.id} comment={comment} />
 					))}
-					<p onClick={switchingGeneric}>volver</p>
+					<p className='post--card--response--container--parraf' onClick={switchingGeneric}>Cerrar resuestas</p>
 				</div>
 			)}
 		</div>
