a50d878608da1ee8861f6082de8014438a7a770d
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-08-21 19:49:50 +0200

    agregando funcionalidad de updateUser

--- components/User/Profile/Profile.jsx
+++ components/User/Profile/Profile.jsx
@@ -3,19 +3,34 @@ import './Profile.css';
 import { usePost } from '../../../hooks/usePost';
 import { useParams } from 'react-router-dom';
 import Loading from '../../Loading/Loading';
+import { useUser } from '../../../hooks/useUser';
 
 export default function Profile() {
 	const { userId } = useParams();
 	const { userPosts, getUserPosts, isLoading } = usePost();
+	const { userById, getUserById } = useUser();
 
 	useEffect(() => {
 		getUserPosts({ userId });
+		getUserById({ userId });
 	}, []);
-
-	console.log(userPosts);
+	console.log(userById);
 	return (
 		<>
 			{isLoading && <Loading />}
+			{!isLoading && userById && (
+				<div className='profile--user--details'>
+					<div className='profile--img--container'>
+						<img
+							src={userById.avatar}
+							alt={`La imagen de perfil del usuario: ${userById.username}`}
+							className='profile--user--details--img'
+						/>
+						<p onClick={null}>Editar perfil</p>
+					</div>
+					<h1>{userById.username}</h1>
+				</div>
+			)}
 			{!isLoading && userPosts && (
 				<div className='profile--users--posts'>
 					{userPosts.map((post) => (
--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,3 +1,35 @@
 import { useState } from 'react';
 
 const API_URL = `${import.meta.env.VITE_API_URL}`;
+
+export function useUser() {
+	const [userById, setUserById] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const getUserById = async ({ userId }) => {
+		let isCancelled = false;
+		setIsLoading(true);
+
+		fetch(`${API_URL}/${userId}`)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw response;
+			})
+			.then((data) => {
+				if (!isCancelled) {
+					setUserById(data);
+					setInterval(() => {
+						setIsLoading(false);
+					}, 1000);
+				}
+			})
+			.catch((err) => console.error(err));
+		return () => {
+			isCancelled = true;
+		};
+	};
+
+    return {userById, isLoading, getUserById}
+}
